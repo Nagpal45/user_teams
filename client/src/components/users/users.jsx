42a2ad81bab9dcea31608b1421888c88051bddea
Vaibhav Nagpal <95275684+Nagpal45@users.noreply.github.com>
2024-04-02 01:35:43 +0530

    Update users.jsx

--- client/src/components/users/users.jsx
+++ client/src/components/users/users.jsx
@@ -3,24 +3,18 @@ import Card from '../card/card';
 import axios from 'axios';
 import './users.css';
 import { Error } from '@mui/icons-material';
-import { Loading } from '../loading/loading';
 
 const Users = ({ filters, page, select, handleUserSelect, selectedUsers }) => {
   const [users, setUsers] = useState([]);
   const [warning, setWarning] = useState('');
-  const [loading, setLoading] = useState(true);
 
   const getUsers = async () => {
     try {
-      setLoading(true);
       const res = await axios.get('/api/users', { params: { ...filters, page } });
       const data = await res.data.users;
       setUsers(data);
     } catch (error) {
       console.error('Failed to fetch users:', error);
-    }
-    finally {
-      setLoading(false); 
     }
   };
 
@@ -30,7 +24,6 @@ const Users = ({ filters, page, select, handleUserSelect, selectedUsers }) => {
 
   return (
     <div className='users'>
-      {loading && (<Loading/>)}
       {warning && <div className="warning"><Error/>{warning}</div>}
       {users?.map((user, index) => (
         <Card user={user} key={index} select={select} handleUserSelect={handleUserSelect} selectedUsers={selectedUsers} setWarning={setWarning} getUsers={getUsers}/>
